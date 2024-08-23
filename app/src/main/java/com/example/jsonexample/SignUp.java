746416f4c0579ca9096c2a56f9b2aa27e5472080
علي يحيي زكريا <alyyehiazak@gmail.com>
2024-08-23 17:03:04 +0300

    -Welcome Activity design / Signup Activity design / Login Activity design / Made startup activity (welcome) / new images and resource files in drawable folder

--- app/src/main/java/com/example/jsonexample/SignUp.java
+++ app/src/main/java/com/example/jsonexample/SignUp.java
@@ -8,19 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-
 }
--- app/src/main/java/com/example/jsonexample/Welcome.java
+++ app/src/main/java/com/example/jsonexample/Welcome.java
@@ -1,6 +1,8 @@
 package com.example.jsonexample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class Welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_welcome);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -22,5 +24,13 @@ public class Login extends AppCompatActivity {
         });
     }
 
+    public void welcome_login_pressed(View v){
+        Intent intent = new Intent(this,Login.class);
+        startActivity(intent);
+    }
 
+    public void welcome_signup_pressed(View v){
+        Intent intent = new Intent(this,SignUp.class);
+        startActivity(intent);
+    }
 }
